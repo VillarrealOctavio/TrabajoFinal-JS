616b535e4ca119f6479ec6d8ecdd5ba3db66784a
VillarrealOctavio <octavillarreal2016@gmail.com>
2022-02-06 23:41:23 -0300

    Introduciendo DOM al proyecto de JS

--- js/java.js
+++ js/java.js
@@ -1,104 +1,40 @@
-/*QUIERO REALIAR UNA SECCIÓN DONDE EL USUARIO PUEDA COMPRAR PRODUCTOS (EN ESTE CASO GUITARRA) Y, ADEMÁS, LOS MISMOS SE GUARDEN EN EL STOCK. 
-A SU VEZ, QUIERO QUE LOS MISMOS SALGAN DEL STOCK CUANDO EL CLIENTE LOS COMPRE. QUIERO TAMBIÉN GENERAR UN CARRITO
-DE COMPRAS*/
-function precioFinal (parametro1, parametro2){
-    return alert((parametro1*1.21)*parametro2)
+console.dir(document);
+/*let form = document.querySelector(".formulario");
+console.log(form.innerHTML);
+
+let usuario = document.querySelector(".usuario")
+console.log(usuario.innerHTML);
+
+let correo = document.querySelector(".email")
+console.log(correo.innerHTML);
+
+let contra = document.querySelector(".password");
+console.log(contra.innerHTML);*/
+
+/*let bienvenidaIngreso = document.querySelector(".bienvenida");
+bienvenidaIngreso.innerHTML = "<h4>Hola rockero</h4>"*/
+
+
+let bienvenida = document.createElement("div");
+console.log(bienvenida.innerHTML);
+bienvenida.innerHTML = "<h3>HOLA ROCKERO</h3>"
+document.body.appendChild(bienvenida);
+
+
+let listaShirts = document.querySelector(".remeras");
+for(const remera of shirt) {
+    let li = document.createElement("li");
+    let boton = document.createElement("button");
+    li.innerHTML = `${remera.id} - ${remera.estampado} - ${remera.descr} - ${remera.talle} - ${remera.precio}`;
+    boton.innerHTML = "<button>Agregar al carrito</button>"
+    listaShirts.append(li)
+    listaShirts.append(boton)
 }
-function despedida () {
-    alert("Gracias por su compra!")
-}
-alert("Bienvenido al área de compras de guitarras")
-let ingresar = prompt("¿Desea usted comprar una guitarra? Ingresar si o no")
-ingresar.toLowerCase
-while(ingresar==""){
-    alert("El valor ingresado es incorrecto")
-    ingresar=prompt("¿Desea usted comprar una guitarra? Ingresar si o no")
-}
-if(ingresar=="si") {
-    alert("Continúe")
-}else {
-    alert("Entonces puede mirar las mismas y buscar")
-}
-class Guitar {
-    constructor(id, nombre, precio) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-    }
-}
-const guitarra = new Guitar (1, "Gibson SG", 250000)
-const guitarra1 = new Guitar (2, "Gisbon LesPaul", 300000)
-const guitarra2 = new Guitar (3, "Fender Stratocaster", 150000)
-const guitarra3 = new Guitar (4, "Fender Telecaster", 200000)
-const guitarra4 = new Guitar (5, "Gibson SG Standard", 400000)
-const guitarra5 = new Guitar (6, "Gisbon LesPaul 1956", 500000)
-const guitarra6 = new Guitar (7, "Fender Stratocaster 1960", 350000)
-const guitarra7 = new Guitar (8, "Fender Telecaster 2021", 230000)
 
+const carritoCompras = [];
 
-const carritoGuitarras = [];
+let shirtTitle = document.querySelector(".remerasTitulo");
+console.log(shirtTitle.innerHTML);
 
-const BBDD = [
-guitarra,
-guitarra1,
-guitarra2,
-guitarra3,
-guitarra4,
-guitarra5,
-guitarra6,
-guitarra7
-]
-alert("A continuación se le presentará la lista de nuestras guitarras con sus respectivos precios.")
-function mostrarProductos(){
-    BBDD.forEach(element => {
-        console.log(element)
-        alert("id " + element.id + "- nombre " + element.nombre + "- precio " + element.precio + ".")
-    });
-}
-mostrarProductos ();
-
-
-let eleccionGuitarra = parseInt(prompt("Ingrese el id de la guitarra que desea comprar"))
-
-// --------------------------------------------------
-/*function ingresarCompra() {
-    BBDD.find((el)=>el.id==eleccionGuitarra)
-    carritoGuitarras.push(el)
-}*/
-// --------------------------------------------------
-
-
-
-const compraRealizada = BBDD.find((el) => el.id === eleccionGuitarra)
-carritoGuitarras.push(compraRealizada)
-
-for (const el of carritoGuitarras){
-    console.log(el);
-    alert("Usted ha comprado una guitarra " + el.nombre + ".");
-}
-despedida();
-
-
-
-
-// ---------------------------------------------------------------------------------------------------------------------------------------
-// Este código lo dejo comentado. (Funciona bien pero hay que eliminar parte del código de arriba para que se ejecute correctamente.)
-// function comprar (){
-//     for (const el of BBDD){
-//         carritoGuitarras.push(el);
-//         console.log(carritoGuitarras);
-//     }
-// }
-// comprar();
-// function ordenarPrecios(){
-//     BBDD.sort((a,b)=>{
-//         return a.precio - b.precio
-//     }) 
-// }
-// ordenarPrecios();
-// console.log(BBDD);
-// const masBarato = BBDD.filter((el)=>el.precio<320000);
-// console.log(masBarato);
-
-
+shirtTitle.innerHTML = "<h3>Bienvenido a nuestro sistema de compras!</h3>"
 
---
+++ js/productos.js
@@ -0,0 +1,27 @@
+// REMERAS
+class Shirt {
+    constructor(id, estampado, descr, precio){
+        this.id = id;
+        this.estampado = estampado;
+        this.descr = descr;
+        this.precio = precio
+    }
+}
+const shirt = [
+{id:1, estampado:"angus", descr:"A rockear", talle: "S", precio: 2000},
+{id:2, estampado:"banda", descr:"A rockear", talle: "S", precio: 1100},
+{id:3, estampado:"malcom", descr:"A rockear", talle: "M", precio: 1850},
+{id:4, estampado:"guitarra", descr:"A rockear", talle: "M", precio: 1260},
+{id:5, estampado:"angus", descr:"A rockear", talle: "L", precio: 2000},
+{id:6, estampado:"banda", descr:"A rockear", talle: "L", precio: 1100},
+{id:7, estampado:"malcom", descr:"A rockear", talle: "XL", precio: 1850},
+{id:8, estampado:"guitarra", descr:"A rockear", talle: "XL", precio: 1260}
+]
+
+// GUITARRAS
+
+// TAZAS
+
+
+
+// DISCOS
