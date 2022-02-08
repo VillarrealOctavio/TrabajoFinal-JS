2a9faa73d21c305624759b9f2d84ae32ddd64a74
VillarrealOctavio <octavillarreal2016@gmail.com>
2022-02-08 11:27:10 -0300

    Incluyendo Eventos con DOM al proyecto de JS

--- js/java.js
+++ js/java.js
@@ -1,24 +1,6 @@
-console.dir(document);
-/*let form = document.querySelector(".formulario");
-console.log(form.innerHTML);
-
-let usuario = document.querySelector(".usuario")
-console.log(usuario.innerHTML);
-
-let correo = document.querySelector(".email")
-console.log(correo.innerHTML);
-
-let contra = document.querySelector(".password");
-console.log(contra.innerHTML);*/
-
-/*let bienvenidaIngreso = document.querySelector(".bienvenida");
-bienvenidaIngreso.innerHTML = "<h4>Hola rockero</h4>"*/
-
-
-let bienvenida = document.createElement("div");
+let bienvenida = document.querySelector(".bienvenida");
 console.log(bienvenida.innerHTML);
 bienvenida.innerHTML = "<h3>HOLA ROCKERO</h3>"
-document.body.appendChild(bienvenida);
 
 
 let listaShirts = document.querySelector(".remeras");
@@ -38,3 +20,92 @@ console.log(shirtTitle.innerHTML);
 
 shirtTitle.innerHTML = "<h3>Bienvenido a nuestro sistema de compras!</h3>"
 
+
+
+
+
+
+
+
+
+
+
+// -----------------------------------------------------------------------------------------------------------
+
+
+
+// Código de guitarras
+let seleccionGuitar = document.querySelector("#selectGuitar")
+let containerGuitar = document.querySelector(".contenedor-guitar")
+
+seleccionGuitar.addEventListener('change', ()=> {
+    console.log (seleccionGuitar.value)
+    if(seleccionGuitar.value=="all") {
+        showProducts(Guitar)
+    }else{
+        showProducts(Guitar.filter(el =>el.marca==seleccionGuitar.value))
+    }
+})
+
+showProducts(Guitar);
+
+function showProducts(array) {
+    containerGuitar = "";
+    array.forEach(el => {
+        let div = document.createElement("div")
+        div.className=`col-md-4`;
+        div.innerHTML = `
+                    <div class="card" style="width: 18rem;">
+                    <img src="${el.img}" class="card-img-top" alt="Guitar">
+                    <div class="card-body">
+                    <h5 class="card-title">${el.marca}</h5>
+                    <p class="card-text">${el.modelo}</p>
+                    <p class="card-text">${el.precio}</p>
+                    </div>
+                    </div>
+        `       
+        containerGuitar.appendChild(div); 
+    });
+}
+
+
+
+
+
+
+// -------------------------------------------------------------------------------------------------------
+
+
+
+
+// CÓDIGO DE BANDA
+
+
+
+const integrantesBanda = [
+    {nombre:"Angus Young", puesto: "Guitarra Principal",img:`/assets/img/angus.jfif`},
+    {nombre:"Malcom Young", puesto: "Guitarra Rítmica",img:`/assets/img/Malcom Young.jfif`},
+    {nombre:"Bryan Jonhson", puesto: "Voz",img:`/assets/img/bryan.jfif`},
+    {nombre:"Phil Rudd", puesto: "Bateria",img:`/assets/img/Phil Rudd.jfif`},
+    {nombre:"Cliff Williams", puesto: "Bajo",img:`/assets/img/Cliff Williams.jfif`}
+];
+
+
+
+let mostrarBanda = document.querySelector(".showBand");
+let banda = document.querySelector("#acdcBand");
+
+showBand.addEventListener("click", showAcdcBand(integrantesBanda))
+
+showAcdcBand(integrantesBanda);
+
+function showAcdcBand (array){
+    for (const el of array) {
+        let div = document.createElement("div");
+        div.className="col-md-12";
+        div.innerHTML= 
+        `<img src="${el.img}" alt="${el.nombre}">
+        <h4>${el.nombre}<span class="badge badge-secondary">${el.puesto}</span></h4>`
+        banda.appendChild(div);
+    }
+}
--- js/productos.js
+++ js/productos.js
@@ -20,6 +20,33 @@ const shirt = [
 
 // GUITARRAS
 
+const Guitar = [
+    {id: 1, marca: "Gibson", modelo: "SG", precio: 500000, img:`/assets/img/guitarra.jpg`},
+    {id: 2, marca: "Gibson", modelo: "LesPaul", precio: 700000},
+    {id: 3, marca: "Gibson", modelo: "Flaying V", precio: 4500000},
+    {id: 4, marca: "Gretsch", modelo: "G6659TG", precio: 380000},
+    {id: 5, marca: "Gretsch", modelo: "G5420T", precio: 395000},
+    {id: 6, marca: "Fender", modelo: "Strato Afinnity", precio: 260000},
+    {id: 7, marca: "Fender", modelo: "Telecaster", precio: 300000},
+    {id: 8, marca: "Fender", modelo: "Telecaster 1960", precio: 510000},
+    {id: 9, marca: "Ibañez", modelo: "JEM", precio: 150000},
+    {id: 10, marca: "Ibañez", modelo: "GRX20", precio: 190000},
+    {id: 11, marca: "Gibson", modelo: "SG", precio: 500000},
+    {id: 12, marca: "Gibson", modelo: "LesPaul", precio: 700000},
+    {id: 13, marca: "Gibson", modelo: "Flaying V", precio: 4500000},
+    {id: 14, marca: "Gretsch", modelo: "G6659TG", precio: 380000},
+    {id: 15, marca: "Gretsch", modelo: "G5420T", precio: 395000},
+    {id: 16, marca: "Fender", modelo: "Strato Afinnity", precio: 260000},
+    {id: 17, marca: "Fender", modelo: "Telecaster", precio: 300000},
+    {id: 18, marca: "Fender", modelo: "Telecaster 1960", precio: 510000},
+    {id: 19, marca: "Ibañez", modelo: "JEM", precio: 150000},
+    {id: 20, marca: "Ibañez", modelo: "GRX20", precio: 190000},
+    {id: 21, marca: "Gibson", modelo: "SG", precio: 500000, img:"/assets//img/guitarra.jpg"},
+    {id: 22, marca: "Gibson", modelo: "LesPaul", precio: 700000},
+    {id: 23, marca: "Gibson", modelo: "Flaying V", precio: 4500000},
+    {id: 24, marca: "Gretsch", modelo: "G6659TG", precio: 380000}
+] 
+
 // TAZAS
 
 
